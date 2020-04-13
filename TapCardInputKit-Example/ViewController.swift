ac3b8d1344770a718b70823424ca38bc157caa3e
Tap Payments <o.rabie@tap.company>
2020-04-13 08:19:51 +0200

    1- Preping the example project

--- TapCardInputKit-Example/ViewController.swift
+++ TapCardInputKit-Example/ViewController.swift
@@ -12,19 +12,13 @@ import class CommonDataModelsKit_iOS.TapCard
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cardFull: TapCardInput!
-    @IBOutlet weak var cardInline: TapCardInput!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        cardFull.setup(for: .FullCardInput)
-        cardFull.setCardData(tapCard: .init(tapCardNumber: "4242424242424242", tapCardExpiryMonth: "12",tapCardExpiryYear: "20", tapCardCVV: "11"))
-        //cardInline.setup(for: .InlineCardInput)
     }
 
 
